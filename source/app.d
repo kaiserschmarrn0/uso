55f1239c9394d443e172b97a573d8bf3a89b38cf
kaiserschmarrn_ <kaiserschmarrn0@gmail.com>
2019-09-02 00:42:06 -0400

    nicest

--- source/app.d
+++ source/app.d
@@ -7,6 +7,8 @@ import bindbc.opengl;
 
 nothrow:
 
+GLFWwindow* win;
+
 extern (C) void uso_glfw_error_cb(int code, const(char)* dsc) {
 	printf("uso: glfw error:\n\terror code: %d\n\terror desc: %s\n", code, dsc);
 }
@@ -27,6 +29,10 @@ extern (C) void uso_scroll_cb(GLFWwindow* win, double xoff, double yoff) {
 	printf("uso: scroll:\n\txoff: %lf\n\tyoff: %lf\n", xoff, yoff);
 }
 
+extern (C) void uso_fb_size_cb(GLFWwindow* win, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
 extern (C) void uso_drop_cb(GLFWwindow* win, int count, const char** paths) {
 	printf("uso: drop:\n\tcount: %d\n", count);
 	for (uint i = 0; i < count; i++) {
@@ -34,8 +40,13 @@ extern (C) void uso_drop_cb(GLFWwindow* win, int count, const char** paths) {
 	}
 }
 
-extern (C) void uso_fb_size_cb(GLFWwindow* win, int width, int height) {
-	glViewport(0, 0, width, height);
+extern (C) void uso_key_cb(GLFWwindow* win, int key, int scancode, int action, int mods) {
+	//GLFW_PRESS GLFW_RELEASE GLFW_REPEAT
+	//GLFW_KEY_UNKNOWN
+	printf("uso: keyboard:\n\tkey: %d\n\tscancode: %d\n\taction: %d\n\tmods: %d\n", key, scancode, action, mods);
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(win, true);
+	}
 }
 
 void main() {
@@ -52,7 +63,7 @@ void main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* win = glfwCreateWindow(640, 480, "uso!", null, null);
+	win = glfwCreateWindow(640, 480, "uso!", null, null);
 	glfwMakeContextCurrent(win);
 	glfwSwapInterval(0);
 
@@ -65,107 +76,95 @@ void main() {
 	}
 
 	glViewport(0, 0, 640, 480);
-
 	glfwSetFramebufferSizeCallback(win, &uso_fb_size_cb);
-
 	glfwSetWindowCloseCallback(win, &uso_win_close_cb);
-
-	extern (C) void uso_key_cb(GLFWwindow* win, int key, int scancode, int action, int mods) {
-		//GLFW_PRESS GLFW_RELEASE GLFW_REPEAT
-		//GLFW_KEY_UNKNOWN
-		printf("uso: keyboard:\n\tkey: %d\n\tscancode: %d\n\taction: %d\n\tmods: %d\n", key, scancode, action, mods);
-		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-			glfwSetWindowShouldClose(win, true);
-		}
-	}
-
 	glfwSetKeyCallback(win, &uso_key_cb);
 	glfwSetCursorPosCallback(win, &uso_cursor_pos_cb);
 	glfwSetMouseButtonCallback(win, &uso_mouse_button_cb);
 	glfwSetScrollCallback(win, &uso_scroll_cb);
 	glfwSetDropCallback(win, &uso_drop_cb);
 
-	const char *vertex_shader_source =
+	const char *vertex_shader_src = 
 		"#version 460 core\n
 		layout (location = 0) in vec3 aPos;\n
 		void main(){\n
 			gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n
 		}\0";
 
-	const char *fragment_shader_source =
+	const char *fragment_shader_src =
 		"#version 460 core\n
 		out vec4 FragColor;\n
 		void main(){\n
 			FragColor = vec4(0f, .5f, .2f, 1f);\n
 		}\0";
 
-	const uint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_source, null);
-	glCompileShader(vertex_shader);
+	bool setup_shader(const char *name, GLenum type, const char *src, uint *shader) {
+		const uint loc = glCreateShader(type);
+		*shader = loc;
+		glShaderSource(loc, 1, &src, null);
+		glCompileShader(loc);
 
-	int success;
-	char[512] info;
-	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertex_shader, 512, null, info.ptr);
-		printf("uso: failed to compile vertex shader: %512s\n", info.ptr);
+		int success;
+		glGetShaderiv(loc, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			char[512] info;
+			glGetShaderInfoLog(loc, 512, null, info.ptr);
+			printf("uso: failed to compile %s shader: %512s\n", name, info.ptr);
+			return true;
+		}
+		return false;
 	}
 
-	const uint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_source, null);
-	glCompileShader(fragment_shader);
-
-	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragment_shader, 512, null, info.ptr);
-		printf("uso: failed to compile fragment shader: %512s\n", info.ptr);
+	uint vertex_shader;
+	if (setup_shader("vertex", GL_VERTEX_SHADER, vertex_shader_src, &vertex_shader)) {
+		goto out1;
 	}
 
+	uint fragment_shader;
+	if (setup_shader("fragment", GL_FRAGMENT_SHADER, fragment_shader_src, &fragment_shader)) {
+		goto out1;
+	}
+	
 	const uint shader_program = glCreateProgram();
 	glAttachShader(shader_program, vertex_shader);
 	glAttachShader(shader_program, fragment_shader);
 	glLinkProgram(shader_program);
-
+	int success;
 	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shader_program, 512, null, info.ptr);
-		printf("uso: failed to link shader program: %512s\n", info.ptr);
-	}
 	glDeleteShader(vertex_shader);
 	glDeleteShader(fragment_shader);
+	if (!success) {
+		char[512] info;
+		glGetProgramInfoLog(shader_program, 512, null, info.ptr);
+		printf("uso: failed to link shader program: %512s\n", info.ptr);
+		goto out1;
+	}
 
-	const float[12] vertices = [
-		.5f, .5f, 0f,
-		.5f, -.5f, 0f,
-		-.5f, -.5f, 0f,
-		-.5f, .5f, 0f
-	];
-	
-	const uint[6] indices = [
-		0, 1, 3,
-		1, 2, 3
-	];
+	const float[12] vertices = [ .5f, .5f, 0f, .5f, -.5f, 0f, -.5f, -.5f, 0f, -.5f, .5f, 0f ];
+	const uint[6] indices = [ 0, 1, 3, 1, 2, 3 ];
 
-	
 	uint vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	void setup_buffer(GLuint* bo, GLenum type, void* data, size_t len, GLenum usage) {
+		glGenBuffers(1, bo);
+		glBindBuffer(type, *bo);
+		glBufferData(type, len, data, usage);
+	}
+
 	uint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, vertices.sizeof, cast(const(void)*)vertices, GL_STATIC_DRAW);
+	setup_buffer(&vbo, GL_ARRAY_BUFFER, cast(void*)vertices.ptr, vertices.sizeof, GL_STATIC_DRAW);
 
 	uint ebo;
-	glGenBuffers(1, &ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.sizeof, cast(const(void)*)indices, GL_STATIC_DRAW);
+	setup_buffer(&ebo, GL_ELEMENT_ARRAY_BUFFER, cast(void*)indices.ptr, indices.sizeof, GL_STATIC_DRAW);
 
+	//info about vertex array
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * float.sizeof, cast(void*)0);
 	glEnableVertexAttribArray(0);
 
+	//unbind stuff
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
 	glBindVertexArray(0);
 
 	enum double fps = 60;
@@ -184,10 +183,11 @@ void main() {
 
 		glfwSwapBuffers(win);
 
-		const Duration end = start - MonoTime.currTime;
-		if (spf >= end) {
-			Thread.sleep(spf - end);
-		}
+		Duration end;
+		do {
+			end = MonoTime.currTime - start;
+			glfwWaitEventsTimeout(cast(double)end.total!"usecs" / 1_000_000f);
+		} while(spf >= end);
 	}
 
 	glDeleteVertexArrays(1, &vao);
@@ -196,10 +196,7 @@ void main() {
 
 out1:
 	glfwDestroyWindow(win);
-
 	glfwTerminate();
-
 	printf("uso: exiting.\n");
-
 	return;
 }
